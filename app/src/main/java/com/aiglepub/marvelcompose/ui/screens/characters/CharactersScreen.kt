4a305dcb6b420dfdbf61346f99e3f4784005106c
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-12 13:50:34 +0200

    Add onClick in item

--- app/src/main/java/com/aiglepub/marvelcompose/ui/screens/characters/CharactersScreen.kt
+++ app/src/main/java/com/aiglepub/marvelcompose/ui/screens/characters/CharactersScreen.kt
@@ -1,6 +1,7 @@
 package com.aiglepub.marvelcompose.ui.screens.characters
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -17,6 +18,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,9 +33,9 @@ import com.aiglepub.marvelcompose.data.entities.Character
 import com.aiglepub.marvelcompose.data.repositories.CharactersRepository
 
 @Composable
-fun CharactersScreen() {
+fun CharactersScreen(onClick: (Character) -> Unit) {
 
-    var charactersState by remember { mutableStateOf(emptyList<Character>()) }
+    var charactersState by rememberSaveable { mutableStateOf(emptyList<Character>()) }
 
     // En la key le paso el argumento que si cambia se volveria a ejecutar
     // Si le pongo TRUE eso es un estado que no cambia y solo se ejecuta 1 vez
@@ -41,27 +43,32 @@ fun CharactersScreen() {
         charactersState = CharactersRepository.getCharacters()
     }
 
-
-    CharactersScreenContent(characters = charactersState)
+    CharactersScreenContent(
+        characters = charactersState,
+        onClick = onClick
+    )
 }
 
 @Composable
-fun CharactersScreenContent(characters: List<Character>) {
+fun CharactersScreenContent(characters: List<Character>, onClick: (Character) -> Unit) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(180.dp),
         contentPadding = PaddingValues(4.dp)
     ) {
         items(characters) {
-            CharacterItem(character = it)
+            CharacterItem(
+                character = it,
+                modifier = Modifier.clickable { onClick(it) }
+            )
         }
     }
 }
 
 @Composable
-fun CharacterItem(character: Character) {
+fun CharacterItem(character: Character, modifier: Modifier = Modifier) {
 
     Column(
-        modifier = Modifier.padding(8.dp)
+        modifier = modifier.padding(8.dp)
     ) {
         Card {
             AsyncImage(
@@ -91,6 +98,6 @@ fun CharacterItem(character: Character) {
 @Composable
 private fun CharactersScreen_Preview() {
     MarvelApp {
-        CharactersScreen()
+        CharactersScreen(onClick = {})
     }
 }
