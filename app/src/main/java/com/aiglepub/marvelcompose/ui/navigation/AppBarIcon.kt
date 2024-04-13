cfb40ddedf9694cbb61a7926112d4d256115dbb8
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-13 20:28:41 +0200

    Agregado composable AppBarIcon

---
+++ app/src/main/java/com/aiglepub/marvelcompose/ui/navigation/AppBarIcon.kt
@@ -0,0 +1,19 @@
+package com.aiglepub.marvelcompose.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun AppBarIcon(
+    imageVector: ImageVector,
+    onClick: () -> Unit,
+    contentDescription: String? = null
+) {
+    IconButton(onClick = onClick) {
+        Icon(imageVector = imageVector, contentDescription = contentDescription)
+    }
+}
