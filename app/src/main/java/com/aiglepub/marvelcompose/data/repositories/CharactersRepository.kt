87676f6442a93b30837458331a1c03064273ad3e
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-13 22:51:05 +0200

    Agregados Repositorios de Comics

--- app/src/main/java/com/aiglepub/marvelcompose/data/repositories/CharactersRepository.kt
+++ app/src/main/java/com/aiglepub/marvelcompose/data/repositories/CharactersRepository.kt
@@ -1,13 +1,11 @@
 package com.aiglepub.marvelcompose.data.repositories
 
+import asCharacter
 import com.aiglepub.marvelcompose.data.entities.Character
 import com.aiglepub.marvelcompose.data.network.api.ApiClient
-import com.aiglepub.marvelcompose.data.network.entities.ApiCharacter
-import com.aiglepub.marvelcompose.data.network.entities.asCharacter
-import com.aiglepub.marvelcompose.data.network.entities.asString
 
-object CharactersRepository {
-
+object CharactersRepository: Repository<Character>() {
+/*
     suspend fun getCharacters(): List<Character> {
         //Obtenemos los resultados de la llamada
         val result = ApiClient.charactersService.getCharacters(0, 100)
@@ -19,5 +17,27 @@ object CharactersRepository {
         val result = ApiClient.charactersService.findCharacter(characterId = characterId)
         return result.data.results.first().asCharacter()
     }
+*/
+suspend fun get(): List<Character> = super.get {
+    ApiClient
+        .charactersService
+        .getCharacters(0, 100)
+        .data
+        .results
+        .map { it.asCharacter() }
+}
+
+    suspend fun find(id: Int): Character = super.find(
+        id,
+        findActionRemote = {
+            ApiClient
+                .charactersService
+                .findCharacter(id)
+                .data
+                .results
+                .first()
+                .asCharacter()
+        }
+    )
 }
 
---
+++ app/src/main/java/com/aiglepub/marvelcompose/data/repositories/ComicsRepository.kt
@@ -0,0 +1,31 @@
+package com.aiglepub.marvelcompose.data.repositories
+
+import asComic
+import com.aiglepub.marvelcompose.data.entities.Comic
+import com.aiglepub.marvelcompose.data.network.api.ApiClient
+import toStringFormat
+
+object ComicsRepository : Repository<Comic>() {
+
+    suspend fun get(format: Comic.Format?): List<Comic> = super.get {
+        ApiClient
+            .comicsService
+            .getComics(0, 100, format?.toStringFormat())
+            .data
+            .results
+            .map { it.asComic() }
+    }
+
+    suspend fun find(id: Int): Comic = super.find(
+        id = id,
+        findActionRemote = {
+            ApiClient
+                .comicsService
+                .findComic(id)
+                .data
+                .results
+                .first()
+                .asComic()
+        }
+    )
+}
